79d29d4454dae7facbaee41596184f79215ee549
NusratJahanNila <nnusratjahan330@gmail.com>
2025-11-23 13:46:10 +0600

    delete parcel request

--- src/Pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/Pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../Hooks/useAuth';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
     // userdata
@@ -9,13 +13,46 @@ const MyParcels = () => {
     // axios
     const axiosSecure = useAxiosSecure();
     // tanstack
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [] ,refetch} = useQuery({
         queryKey: ['myParcels', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user.email}`);
             return res.data;
         }
     })
+
+    // delete
+    const handleParcelDelete = (id) => {
+        console.log(id);
+        // alert
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // fetch delete
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        console.log('after delete: ', res.data);
+                        if (res.data.deletedCount) {
+                            // refresh data on ui
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your parcel request has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div>
             my parcels : {parcels.length}
@@ -33,12 +70,27 @@ const MyParcels = () => {
                     </thead>
                     <tbody>
                         {
-                            parcels.map((parcel,index) => <tr key={parcel._id} className="bg-base-200">
-                                <th>{index+1}</th>
+                            parcels.map((parcel, index) => <tr key={parcel._id} className="bg-base-200">
+                                <th>{index + 1}</th>
                                 <td>{parcel.parcelName}</td>
                                 <td>{parcel.cost}</td>
                                 <td>{'pending'}</td>
-                                <td>{'confirm'}</td>
+                                <td className='space-x-1.5'>
+                                    {/* view */}
+                                    <button className='btn btn-square hover:bg-primary'>
+                                        <FaMagnifyingGlass />
+                                    </button>
+                                    {/* edit */}
+                                    <button className='btn btn-square hover:bg-primary'>
+                                        <FiEdit />
+                                    </button>
+                                    {/* delete */}
+                                    <button
+                                        onClick={() => handleParcelDelete(parcel._id)}
+                                        className='btn btn-square hover:bg-primary'>
+                                        <MdDelete />
+                                    </button>
+                                </td>
                             </tr>)
                         }
 
