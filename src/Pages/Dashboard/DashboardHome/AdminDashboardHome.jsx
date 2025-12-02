42212307c611c98b4c17d8f2d8cf92680389390e
NusratJahanNila <nnusratjahan330@gmail.com>
2025-12-02 13:12:25 +0600

    stats and piecharts on admin dashboard

--- src/Pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import { Legend, Pie, PieChart, Tooltip } from 'recharts';
 
 const AdminDashboardHome = () => {
+    const axiosSecure = useAxiosSecure();
+    const { data: deliveryStats = [] } = useQuery({
+        queryKey: ['delivery-status-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/parcels/delivery-status/stats');
+            return res.data;
+        }
+    })
+    // Pie chart
+    const pieChartData=(data)=>{
+        return data.map(item=>{
+            return {
+                name: item.status,
+                value:item.count
+            }
+        })
+    }
     return (
         <div className=' max-w-6xl mx-auto py-5'>
             <div className="bg-white rounded-2xl p-4">
                 <h2 className='text-2xl text-secondary font-bold'>Admin DashBoard</h2>
+
+                {/* Stats */}
+                <div className="stats shadow">
+                    {
+                        deliveryStats.map(deliveryStat => <div key={deliveryStat._id} className="stat">
+                            <div className="stat-figure text-secondary">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    className="inline-block h-8 w-8 stroke-current"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    ></path>
+                                </svg>
+                            </div>
+                            <div className="stat-title text-xl ">{deliveryStat._id}</div>
+                            <div className="stat-value text-secondary">{deliveryStat.count}</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
+                        </div>)
+                    }
+
+                </div>
+
+                {/* Pie Charts */}
+                <div className="mt-10 w-full h-[400px]">
+                    <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 2 }} responsive>
+                        <Pie
+                            dataKey="value"
+                            startAngle={180}
+                            endAngle={0}
+                            data={pieChartData(deliveryStats)}
+                            cx="50%"
+                            cy="100%"
+                            outerRadius="120%"
+                            fill="#8884d8"
+                            label
+                            isAnimationActive={true}
+                        />
+                        <Legend></Legend>
+                        <Tooltip></Tooltip>
+                    </PieChart>
+                </div>
             </div>
         </div>
     );
